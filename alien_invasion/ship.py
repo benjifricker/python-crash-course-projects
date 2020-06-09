e22947e2f3e244daa56e692b4df41e3426834732
benjifricker <benjifricker@icloud.com>
2020-06-09 14:52:24 -0700

    Fix missing screen_rect attribute in Ship class

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -7,7 +7,8 @@ class Ship:
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.settings = ai_game.settings
-        
+        self.screen_rect = ai_game.screen.get_rect()
+
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
