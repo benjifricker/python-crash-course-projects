dc09b20c56c0fc4b95e62e02d2c2b3fc7373b21a
benjifricker <benjifricker@icloud.com>
2020-06-12 14:56:07 -0700

    Add method to change fleet direction

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -123,6 +123,12 @@ class AlienInvasion:
                 self._change_fleet_direction()
                 break
 
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
