2b82c97b20ebe83013e4691f4d51e1b5b0e06c1a
benjifricker <benjifricker@icloud.com>
2020-06-13 13:29:30 -0700

    Bugfix: Remove self.bg_color from AlienInvasion init function

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -32,9 +32,6 @@ class AlienInvasion:
 
         self._create_fleet()
 
-        # Set the background color.
-        self.bg_color = (230, 230, 230)
-
     def run_game(self):
         """Start the main loop for the game."""
         while True:
