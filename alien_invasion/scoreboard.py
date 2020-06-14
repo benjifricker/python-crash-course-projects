66e8e05505e7d4aa13cc7266dc7166f7ddcae89d
benjifricker <benjifricker@icloud.com>
2020-06-14 15:49:05 -0700

    Bugfix: Fix typo in prep_score()

--- alien_invasion/scoreboard.py
+++ alien_invasion/scoreboard.py
@@ -19,7 +19,7 @@ class Scoreboard:
 
     def prep_score(self):
         """Turn the score into a rendered image."""
-        score_str = str(self.status.score)
+        score_str = str(self.stats.score)
         self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
 
         # Display the score at the top right of the screen.
