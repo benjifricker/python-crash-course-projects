28b34d3ed2ddccda5983cfec3b7bea86b5da6b6c
benjifricker <benjifricker@icloud.com>
2020-06-14 21:53:26 -0700

    Extend show_score to display level

--- alien_invasion/scoreboard.py
+++ alien_invasion/scoreboard.py
@@ -42,11 +42,6 @@ class Scoreboard:
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.score_rect.top
 
-    def show_score(self):
-        """Draw score to the screen."""
-        self.screen.blit(self.score_image, self.score_rect)
-        self.screen.blit(self.high_score_image, self.high_score_rect)
-
     def check_high_score(self):
         """Check to see if ther's a new high score."""
         if self.stats.score > self.stats.high_score:
@@ -63,3 +58,9 @@ class Scoreboard:
         self.level_rect = self.level_image.get_rect()
         self.level_rect.right = self.score_rect.right
         self.level_rect.top = self.score_rect.bottom + 10
+
+    def show_score(self):
+        """Draw scores and level to the screen."""
+        self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
