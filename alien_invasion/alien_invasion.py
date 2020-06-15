d95a143fdd6bdd03c9fb38b6166b5dadf1a78156
benjifricker <benjifricker@icloud.com>
2020-06-15 14:22:18 -0700

    Add self.sb.prep_ships() to _ship_hit()

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -161,8 +161,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
         if self.stats.ships_left > 0:
-            # Decrement ships left.
+            # Decrement ships_left, and update scoreboard.
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
