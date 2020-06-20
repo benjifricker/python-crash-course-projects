47f3e1c435052755233a3199d846a7ba273d076a
benjifricker <benjifricker@icloud.com>
2020-06-20 09:02:40 -0700

    Handle missing data with try except

--- data_visualization/death_valley_highs_lows.py
+++ data_visualization/death_valley_highs_lows.py
@@ -13,11 +13,15 @@ with open(filename) as f:
     dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
-        high = int(row[4])
-        low = int(row[5])
-        dates.append(current_date)
-        highs.append(high)
-        lows.append(low)
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f"Missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 # Plot the high and low temperatures.
 plt.style.use('seaborn')
@@ -27,7 +31,8 @@ ax.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot.
-plt.title("Daily high and low temperatures - 2018", fontsize=24)
+title = "Daily high and low temperatures - 2018\nDeath Valley, CA"
+plt.title(title, fontsize=20)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
