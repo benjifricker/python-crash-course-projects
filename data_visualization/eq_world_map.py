b0686aecc9c66b5eaed52051f7175cc8a1db2859
benjifricker <benjifricker@icloud.com>
2020-06-21 23:09:52 -0700

    Add color to marksers

--- data_visualization/eq_world_map.py
+++ data_visualization/eq_world_map.py
@@ -4,11 +4,11 @@ from plotly.graph_objects import Scattergeo, Layout
 from plotly import offline
 
 # Explore the structure of the data.
-filename = 'data/eq_data_1_day_m1.json'
+filename = 'data/eq_data_30_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
-readable_file = 'data/eq_data_30_day_m1.json'
+readable_file = 'data/readable_eq_data.json'
 with open(readable_file, 'w') as f:
     json.dump(all_eq_data, f, indent=4)
 
@@ -29,7 +29,11 @@ data = [{
     'lon': lons,
     'lat': lats,
     'marker': {
-        'size': [5*mag for mag in mags]
+        'size': [5*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'Viridis',
+        'reversescale': True,
+        'colorbar': {'title': 'Magnitude'},
     }
 }]
 my_layout = Layout(title='Global Earthquakes')
