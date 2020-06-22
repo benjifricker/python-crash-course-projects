79b0794a6a986c0e5371a95d4de8ad2f8ecfde3d
benjifricker <benjifricker@icloud.com>
2020-06-22 14:23:47 -0700

    Modify to pull additional info for labels

--- data_visualization/python_repos_visual.py
+++ data_visualization/python_repos_visual.py
@@ -12,10 +12,15 @@ print(f"Status code: {r.status_code}")
 # Process results.
 response_dict = r.json()
 repo_dicts = response_dict['items']
-repo_names, stars = [], []
+repo_names, stars, labels = [], [], []
 for repo_dict in repo_dicts:
     repo_names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
+
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    label = f"{owner}<br/>{description}"
+    labels.append(label)
 
 # Make visualization.
 data = [{
