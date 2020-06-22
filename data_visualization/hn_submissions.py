69c7e81ae156ec6deba0e8a104fdcd079bbb6dc2
benjifricker <benjifricker@icloud.com>
2020-06-22 14:51:48 -0700

    Dump json from each submission into response_dict

--- data_visualization/hn_submissions.py
+++ data_visualization/hn_submissions.py
@@ -15,3 +15,4 @@ for submission_id in submission_ids[:30]:
     url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
     r = requests.get(url)
     print(f"id: {submission_id}\tstatus: {r.status_code}")
+    response_dict = r.json()
