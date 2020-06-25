0b571cc546da6b4d9aa8925b439d28e133a6e693
benjifricker <benjifricker@icloud.com>
2020-06-25 01:40:16 -0700

    Fix typo in models module

--- web_application/learning_logs/models.py
+++ web_application/learning_logs/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Topic(models.Models):
+class Topic(models.Model):
     """A topic the user is learning about."""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
